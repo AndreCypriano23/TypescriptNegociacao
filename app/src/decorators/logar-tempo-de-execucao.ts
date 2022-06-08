9890c5f0c895e586855e46635846c38a3134af09
AndreCypriano23 <andrecypriano23@outlook.com>
2022-06-08 15:35:33 -0300

    Outro decorator inserido

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao(){
+export function logarTempoDeExecucao(emSegundos: boolean = false){
 
     return function(
         target: any,
@@ -8,14 +8,20 @@ export function logarTempoDeExecucao(){
     {
         const metodoOriginal = descriptor.value; //guardei o método original
         descriptor.value = function(...args: any[]){//aqui vou sobrescrever o comportamento do método original
-            
+
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if(emSegundos){
+                divisor = 1000;
+                unidade = 'segundos';
+            }
+
             const t1 = performance.now();//performance é variavel global do navegador
             //chamar o método original
-
             const retorno = metodoOriginal.apply(this, args);//this da classe que for usar e o args sao os valores do array, parametros
 
             const t2 = performance.now(); 
-            console.log(`${propertyKey}, tempo de execução: ${( t2-t1/1000 )} segundos`)
+            console.log(`${propertyKey}, tempo de execução: ${( t2-t1/divisor )} ${unidade}`)
             retorno
         };
 
